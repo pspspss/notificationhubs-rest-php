b0e318aacb48dc7fabb7f56c1171db968064615c
Paulo Alves <pauloalvsjr@gmail.com>
2019-03-06 14:17:50 -0300

    Update: GcmNotification

--- src/NotificationHubsRest/Notification/GcmNotification.php
+++ src/NotificationHubsRest/Notification/GcmNotification.php
@@ -2,25 +2,19 @@
 
 namespace Openpp\NotificationHubsRest\Notification;
 
+/**
+ *
+ * @author shiroko@webware.co.jp
+ *
+ */
 class GcmNotification extends AbstractNotification
 {
-    /**
-     * @var string[]
-     */
-    private $supportedOptions = [
-        'collapse_key',
-        'delay_while_idle',
-        'time_to_live',
-        'restricted_package_name',
-        'dry_run',
-    ];
-
     /**
      * {@inheritdoc}
      */
     public function getFormat()
     {
-        return 'gcm';
+        return "gcm";
     }
 
     /**
@@ -28,7 +22,7 @@ class GcmNotification extends AbstractNotification
      */
     public function getContentType()
     {
-        return 'application/json;charset=utf-8';
+        return "application/json;charset=utf-8";
     }
 
     /**
@@ -36,20 +30,17 @@ class GcmNotification extends AbstractNotification
      */
     public function getPayload()
     {
-        if (!empty($this->options)) {
-            $payload = array_intersect_key($this->options, array_fill_keys($this->supportedOptions, 0));
-        } else {
-            $payload = [];
-        }
+        try {
+          $payload = [
+            'data' => array_merge([
+                'message' => $this->alert
+            ], $this->options)
+          ];
 
-        if (is_array($this->alert)) {
-            $payload += ['data' => $this->alert];
-        } elseif (is_scalar($this->alert)) {
-            $payload += ['data' => ['message' => $this->alert]];
-        } else {
-            throw new \RuntimeException('Invalid alert.');
-        }
+          return json_encode($payload);
 
-        return json_encode($payload);
+        } catch(\Exception $e) {
+            throw new \RuntimeException('Invalid alert.'.$e);
+        }
     }
 }
